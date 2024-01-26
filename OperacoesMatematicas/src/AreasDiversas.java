5502c94622098eb526e4b1464fa9f48927ed61e2
Mestre <kadukitesesi@gmail.com>
2024-01-25 21:41:04 -0300

    estudo do dia

---
+++ OperacoesMatematicas/src/AreasDiversas.java
@@ -0,0 +1,13 @@
+public class AreasDiversas {
+
+    static final double pi = 3.14159265358979323846;
+    public static double calculaAreaQuadrado(double ladoQuadrado) {
+        return  ladoQuadrado * ladoQuadrado;
+    }
+
+    public static double calculaAreaCirculo(double raio) {
+
+        return (raio * raio) * pi;
+    }
+
+}
