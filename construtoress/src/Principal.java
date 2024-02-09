aa1b22323201f501d64f49a271c5fc65a485cd35
Mestre <kadukitesesi@gmail.com>
2024-02-08 22:48:19 -0300

    Estudos sobre construtores

--- construtoress/src/Principal.java
+++ construtoress/src/Principal.java
@@ -1,6 +1,7 @@
 public class Principal {
     public static void main(String[] args) {
-        Produto produto1 = new Produto(null,0);
+        Produto produto1 = new Produto("Carlos",1000);
         System.out.println(produto1.nome);
+        System.out.println(produto1.quantidadeEstoque);
     }
 }
