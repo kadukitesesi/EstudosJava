a20c41cf9055c0550f251abf9e57d4d076b9695c
Mestre <kadukitesesi@gmail.com>
2024-02-13 21:02:38 -0300

    arendizado

---
+++ pagamendoConta/src/com/kadukitesesi/financeiro/Fornecedor.java
@@ -0,0 +1,25 @@
+package com.kadukitesesi.financeiro;
+
+import java.io.Serializable;
+
+    public class Fornecedor implements Serializable {
+        private String nome;
+
+        public Fornecedor() {
+        }
+
+        public Fornecedor(String nome) {
+            this.nome = nome;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+    }
+
+
